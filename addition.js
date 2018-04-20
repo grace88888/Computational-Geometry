856df017cfa71a6f5fabd9bfd27d5983eea1fbb7
grace88888 <grace.leung19@ycdsbk12.ca>
2018-04-20 13:54:40 -0400

    Update and rename additions.html to addition.js

--- addition.js
+++ addition.js
@@ -20,3 +20,24 @@ var intersect = closestIntersect;
   ctx.moveTo(intersect.x,360);
   ctx.lineTo(0,0);
   ctx.stroke();
+
+//////PARALLELL HALF
+var intersect = closestIntersect;
+  // Draw red laser
+  ctx.strokeStyle = "#dd3838";
+  ctx.beginPath();
+  ctx.moveTo(320,180);
+  ctx.lineTo(intersect.x,intersect.y);
+  
+  ctx.stroke();
+
+  X = 320 - (intersect.x)*(180 - intersect.y)/(320 - intersect.x)
+  ctx.strokeStyle = "#dd3838";
+  ctx.beginPath();
+  ctx.moveTo(intersect.x,360);
+  ctx.lineTo(0,X);
+  ctx.stroke();
+  ctx.fillStyle = "#dd3838";
+  ctx.beginPath();
+    ctx.arc(intersect.x, intersect.y, 4, 0, 2*Math.PI, false);
+    ctx.fill();
